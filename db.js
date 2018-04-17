22ec2ea75627c35a91406b7426813d33a6b6d327
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2018-04-17 07:03:28 +0200

    Add mysql connection testing code

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql      = require('mysql')
+const settings   = require('./db-settings.json')
+const connection = mysql.createConnection(settings)
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
